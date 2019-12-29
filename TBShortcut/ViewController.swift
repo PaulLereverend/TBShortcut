8993ab8c6e5c0d5f443f957b43c835f767d638b1
Paul <paulereverend@gmail.com>
2019-12-29 16:03:40 +0100

    Initial Commit

---
+++ TBShortcut/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TBShortcut
+//
+//  Created by Paul Lereverend on 29/12/2019.
+//  Copyright © 2019 Paul Lereverend. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
